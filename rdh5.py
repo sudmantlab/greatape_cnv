5cd834a523a4ce2ef93ca533ca90a5b1afbec52a
Swetha Ramesh <sweram19@berkeley.edu>
2019-07-24 17:25:44 -0700

    working progress

--- rdh5.py
+++ rdh5.py
@@ -2,45 +2,52 @@ import pysam
 import pysamstats
 import tables
 import numpy as np
-import matplotlib.pyplot as plt
 import argparse
 import time
 
 
-def build_h5(bam):
+def build_h5(bam, window, step, out):
     bam_file = pysam.AlignmentFile(bam, "rb") # will open index file automatically if in the same folder
-    contig_lengths = {x[0]:x[1] for x in zip(bam_file.references, bam_file.lengths)}
     refs = bam_file.references
-    # for r in contig_lengths:
-    t = time.time()
-    carray = pysamstats.load_coverage(bam_file, chrom=refs[0], start=0, end=contig_lengths[refs[0]])
-    print("time to load contig: {}".format(time.time()-t))
-    plt.plot(carray.pos, carray.reads_all)
-    plt.savefig('plot.png', bbox_inches='tight')
+    contig_lengths = {x[0]:x[1] for x in zip(refs, bam_file.lengths)}
+
+    out_file = tables.open_file(out, mode='w')
+
+    for r in contig_lengths.items():
+        chrom_name = r[0]
+        chrom_length = r[1]
+        out_file.create_group(out_file.root, chrom_name)
+
+        t = time.time()
+        cvg_array = pysamstats.load_coverage(bam_file, chrom=chrom_name, start=0, end=chrom_length)
+        print("time to load contig array:", (time.time()-t) / 60, "minutes")
+
+        s = 0
+        e = window
+
+        read_depth = np.array([], dtype=np.float16)
+        start = np.array([], dtype=np.dtype('u4')) # unsigned int of 4 bytes (32 bits)
+        end = np.array([], dtype=np.dtype('u4'))
+        while e < chrom_length:
+            # todo: create array for window % step != 0
+            # todo: create array for window % step == 0
+
+            # save array in hdf5
+            out_file.create_earray(out_file.root.chrom_name, name="read_depth", atom=tables.Float16Atom(), obj=read_depth)
+            out_file.create_earray(out_file.root.chrom_name, name="start", atom=tables.Float16Atom(), obj=start)
+            out_file.create_earray(out_file.root.chrom_name, name="end", atom=tables.Float16Atom(), obj=end)
+
+
     bam_file.close()
 
 
-# samfile = pysam.AlignmentFile("ex1.bam", "rb" )
-# for pileupcolumn in samfile.pileup("chr1", 100, 120): # (contig, start, stop)
-#     print ("\ncoverage at base %s = %s" %
-#            (pileupcolumn.pos, pileupcolumn.n))
-#     for pileupread in pileupcolumn.pileups:
-#         if not pileupread.is_del and not pileupread.is_refskip:
-#             # query position is None if is_del or is_refskip is set.
-#             print ('\tbase in read %s = %s' %
-#                   (pileupread.alignment.query_name,
-#                    pileupread.alignment.query_sequence[pileupread.query_position]))
-#
-#
-# mybam = pysam.AlignmentFile('/path/to/your/bamfile.bam')
-#
-#
-# samfile.close()
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--bam", required=True, help="Path to BAM file")
+    parser.add_argument("--fn_bam", required=True, help="Path to BAM file")
+    parser.add_argument("--fn_out", required=True, help="Pathname to output file")
+    parser.add_argument("--window", help="Window size", default=1000)
+    parser.add_argument("--step", help="Step size", default=500)
     o = parser.parse_args()
-    build_h5(o.bam)
+    build_h5(o.fn_bam, o.window, o.step, o.fn_out)
 
 
