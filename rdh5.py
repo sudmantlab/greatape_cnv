8f48213ad7c5d0a2614b3322ef7625c9d7f187bb
Swetha Ramesh <sweram19@berkeley.edu>
2019-07-30 17:26:12 -0700

    fixed some more errors

--- rdh5.py
+++ rdh5.py
@@ -4,13 +4,20 @@ import tables
 import numpy as np
 import argparse
 import time
+import math
 
 
 # todo: write better documentation for all of this
-def build_h5(bam, window, step, out):
+def build_h5(bam, window, step, out, chrs):
     bam_file = pysam.AlignmentFile(bam, "rb") # will open index file automatically if in the same folder
     refs = bam_file.references
     contig_lengths = {x[0]:x[1] for x in zip(refs, bam_file.lengths)}
+
+    if len(chrs) != 0:
+        temp = {}
+        for c in chrs:
+            temp[c] = contig_lengths[c]
+        contig_lengths = temp
 
     out_file = tables.open_file(out, mode='w')
 
@@ -20,8 +27,6 @@ def build_h5(bam, window, step, out):
         print("Chromosome name:", chrom_name)
         print("chromosome length:", chrom_length)
         chrom_group = out_file.create_group(out_file.root, chrom_name)
-        if chrom_length == 0:
-            continue
 
         # Generate array of read-depth per base using pysamstats
         # cvg_array.dytpe = dtype((numpy.record, [('chrom', 'S27'), ('pos', '<i4'), ('reads_all', '<i4'), ('reads_pp', '<i4')]))
@@ -42,32 +47,33 @@ def build_h5(bam, window, step, out):
         print("Time to load coverage array for", chrom_name, ":", (time.time()-t) / 60, "minutes")
 
 
-        size = chrom_length // step - window // step + 1 + 1 # number of windows in chrom plus one extra
+        size = math.ceil(chrom_length // step - window // step + 1) # number of windows in chrom
+        if chrom_length < window:
+            size = 1
         print("Number of windows in chrom {}: {}".format(chrom_name, size))
         n = window // step # number of steps in each window
         read_depth = np.zeros(size, dtype=np.float16)
-        start = np.zeros(size, dtype=np.int32) # unsigned int of 4 bytes (32 bits)
-        end = np.zeros(size, dtype=np.int32)
+        start = np.zeros(size, dtype=np.uint32)
+        end = np.zeros(size, dtype=np.uint32)
 
-        # todo: test whether this gives a significant speedup
-        # https://www.geeksforgeeks.org/window-sliding-technique/
         if window % step == 0:
             # an array that records averages for each block of size 'step' in 'chrom'
-            step_arr = np.zeros(chrom_length // step + 1, dtype=np.float16)
-            s, e = 0, step
+            step_arr = np.zeros(math.ceil(chrom_length / step), dtype=np.float16)
+            s = 0
             for i in range(0, chrom_length // step):
-                step_arr[i] = np.mean(cvg[s:e]) / n # dividing by 'n' for faster mean calculation in next for loop
-                if i < size:  # step_arr is longer than start and end
+                step_arr[i] = np.sum(cvg[s:s + step]) / window # dividing by 'window' for faster mean calculation in next for loop
+                if i < size:  # 'step_arr' is longer than 'start' and 'end'
                     start[i] = s
                     end[i] = (s + window if s + window < chrom_length else chrom_length)
-                s, e = e, e + step
+                s += step
             # tail case
-            step_arr[chrom_length // step] = (np.mean(cvg[e:]))
+            if s < chrom_length:
+                step_arr[step_arr.size - 1] = (np.sum(cvg[s:])) / window
             # start[size - 1] = s
             # end[size - 1] = chrom_length
 
             # initial mean calculation (tail case but it's at the beginning)
-            mean = np.mean(step_arr[0:n])
+            mean = np.sum(step_arr[0:n])
             read_depth[0] = mean
             for i in range(1, read_depth.size):
                 mean = mean - step_arr[i - 1] + step_arr[n]
@@ -105,7 +111,19 @@ if __name__ == "__main__":
     parser.add_argument("--fn_out", required=True, help="Pathname to output file")
     parser.add_argument("--window", default=1000, help="Window size")
     parser.add_argument("--step", default=500, help="How much window should slide by")
+    parser.add_argument("--chr", nargs='+', default=[], help="Chromosome(s) to generate read depths for")
     o = parser.parse_args()
-    build_h5(o.fn_bam, o.window, o.step, o.fn_out)
+    if o.window < o.step:
+        print("Window must be greater than step")
+        exit(1)
+    if o.window == 0:
+        print("Window cannot equal zero")
+        exit(1)
+    if o.step == 0:
+        print("Step cannot equal zero")
+        exit(1)
+    if o.fn_out[len(o.fn_out) - 3:] is not ".h5":
+        print("Include \'.h5\' extension at the end of output file name")
+    build_h5(o.fn_bam, o.window, o.step, o.fn_out, o.chr)
 
 
